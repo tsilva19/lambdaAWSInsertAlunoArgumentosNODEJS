4fa975636a2ebe795675440660bdfcbda110bbc9
Thiago <thiagoalves060@gmail.com>
2023-02-24 05:53:33 -0300

    insert de alunos

---
+++ index.js
@@ -0,0 +1,35 @@
+const mysql = require('mysql');
+
+exports.handler = (event, context, callback) => {
+  // Configurações de conexão com o banco de dados MySQL
+  const connection = mysql.createConnection({
+    host: 'localhost',
+    user: 'username',
+    password: 'password',
+    database: 'database_name'
+  });
+
+  // Dados do aluno a ser inserido na tabela de alunos
+  const aluno = {
+    nome: 'João Silva',
+    idade: 25,
+    email: 'joao.silva@gmail.com'
+  };
+
+  // Conexão com o banco de dados MySQL
+  connection.connect();
+
+  // Consulta SQL para inserir um aluno na tabela de alunos
+  connection.query('INSERT INTO alunos SET ?', aluno, function (error, results, fields) {
+    if (error) throw error;
+    console.log(results);
+    // Fechar a conexão com o banco de dados MySQL
+    connection.end();
+    // Retornar a resposta com o status code 201
+    const response = {
+      statusCode: 201,
+      body: 'Aluno inserido com sucesso!'
+    };
+    callback(null, response);
+  });
+};
